4e43d1ff70867a4649ce3bc6fbaa06093072d224
Othman Assas <othman_assas@hotmail.fr>
2019-07-18 07:11:33 +0100

    broken homepage into smaller components, finished styling menu-item component

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../components/directory/Directory';
+
+const HomePage = () => {
+    return (
+        <Directory/>
+    );
+};
+
+export default HomePage;
